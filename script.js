4c8af14a2b86f94e10a118f20cfb6b818c644a11
MONA / <121730102+TempMeow@users.noreply.github.com>
2024-12-15 23:57:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const buttons = document.querySelectorAll('.redirect-button');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    button.textContent = 'Redirecting...'; // Provide feedback to the user
+    button.textContent = 'Opening...'; // Provide feedback to the user
     setTimeout(() => {
-      window.location.href = button.dataset.url; // Redirect after a short delay
+      window.open(button.dataset.url, '_blank'); // Open link in a new tab
     }, 500);
   });
 });
